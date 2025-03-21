7819ac309d5455947f2903e297403174997a3c4e
MusashiSaito <mu97.job@gmail.com>
2025-03-21 22:23:15 +0900

    Initial commit

---
+++ test/sns-sqs-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SnsSqsLambda from '../lib/sns-sqs-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sns-sqs-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SnsSqsLambda.SnsSqsLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
