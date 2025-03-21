080543b17a00a4f9fafcde727732678baca73db0
MusashiSaito <mu97.job@gmail.com>
2025-03-21 22:28:10 +0900

    init

--- lib/sns-sqs-lambda-stack.ts
+++ lib/sns-sqs-lambda-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Topic } from "aws-cdk-lib/aws-sns";
+import { SqsSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
+import { Queue } from "aws-cdk-lib/aws-sqs";
+import { Construct } from "constructs";
+import path = require("path");
 
 export class SnsSqsLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const topic = new Topic(this, "topic", {
+      topicName: "testTopic",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SnsSqsLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new Queue(this, "queue", {
+      queueName: "testQueue",
+    });
+
+    topic.addSubscription(new SqsSubscription(queue));
+
+    const lambdaFunction = new lambda.Function(this, "lambda", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, `../lambda/index.ts`)),
+      handler: 'index.handler',
+      timeout: cdk.Duration.seconds(10),
+    });  
+    lambdaFunction.addEventSource(new SqsEventSource(queue));
   }
 }
